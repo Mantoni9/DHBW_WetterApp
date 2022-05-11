5d6106eca7a94128bcf328bb23e5a21ae80772c3
AMarkic <antonio.markic@kumasoft.de>
2022-05-11 18:52:39 +0200

    added ionic storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicStorageModule} from '@ionic/storage-angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [AppComponent],
